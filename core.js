385731b2ff8887bf1819494e1a104d078e692425
Adam Eivy <atomantic@gmail.com>
2013-07-19 19:54:00 +0000

    adding the yeoman build files to gh-pages [ci skip]

--- core.js
+++ core.js
@@ -1,6 +1,32 @@
 /*! core - v0.1.0 - 2013-07-19
 * https://github.com/atomantic/core.js
 * Copyright (c) 2013 Adam Eivy (@antic); Licensed MIT */
+/*jslint jquery:true*/
+(function($) {
+  
+  /**
+   * convert a form's name/value pairs to a json object
+   */
+  $.fn.formToObject = function() {
+      var o = {},
+          a = this.serializeArray(),
+          name;
+      $.each(a, function() {
+        name = this.name;
+          if (o[name] !== undefined) {
+              if (!o[name].push) {
+                  o[name] = [o[name]];
+              }
+              o[name].push(this.value || '');
+          } else {
+              o[name] = this.value || '';
+          }
+      });
+      return o;
+  };
+
+}(jQuery));
+
 /*global exports*/
 /*jslint browser:true*/
 (function(exports) {
