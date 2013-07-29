410b288227b5bf5def082940455d1a560a01070e
Adam Eivy <atomantic@gmail.com>
2013-07-29 22:31:35 +0000

    adding the yeoman build files to gh-pages [ci skip]

--- core.js
+++ core.js
@@ -1,4 +1,4 @@
-/*! core - v0.1.0 - 2013-07-25
+/*! core - v0.1.0 - 2013-07-29
 * https://github.com/atomantic/core.js
 * Copyright (c) 2013 Adam Eivy (@antic); Licensed MIT */
 /**
@@ -17,6 +17,7 @@
  * @license MIT
  */
 (function($) {
+    'use strict';
     /**
      * convert a form's name/value pairs to a json object
      * 
@@ -48,63 +49,78 @@
 }(jQuery));
 /*jslint browser:true*/
 /**
-* Core.js tries to make the web suck less by providing a bunch of
-* tiny methods and library extensions that should be natively available.
-* 
-* @module core
-* @link https://github.com/atomantic/core.js
-* @copyright 2013 Adam Eivy (@antic)
-* @license MIT
-*
-* @param {object} exports The scope to embed all of the core.js methods
-*/
+ * Core.js tries to make the web suck less by providing a bunch of
+ * tiny methods and library extensions that should be natively available.
+ *
+ * @module core
+ * @link https://github.com/atomantic/core.js
+ * @copyright 2013 Adam Eivy (@antic)
+ * @license MIT
+ *
+ * @param {object} exports The scope to embed all of the core.js methods
+ */
 (function(exports) {
 
-  'use strict';
+    'use strict';
 
-  /**
-   * Get the english ordinal suffix for any number
-   
-   * @param {number} n number The number to evaluate
-   * @return {string} The ordinal for that number
-   */
-  exports.ord = function(n) {
-      var sfx = ["th","st","nd","rd"],
-          v = n%100;
-      return sfx[(v-20)%10] || sfx[v] || sfx[0];
-  };
-  /**
-   * Generic empty function to speed up supplying anon empty functions.
-   * If you are using jQuery, you could use $.noop if returning undefined is desireable
-   * but this one is useful for anything requiring a boolean true return
-   *
-   * @return {boolean} true
-   */
-  exports.fn = function(){return true;};
-  /**
-   * empty event handler function, which simply prevents default handling
-   */
-  exports.eFn = function(e){ e.preventDefault(); };
-  
-  /**
-   * get a new function, which runs two functions serially within a given context
-   * 
-   * @example
-   *    var fn = core.fnMore(oldFn,newFn,someObj);
-   *    fn(); // runs oldFn, then newFn in the context of someObj
-   * @param {function} originalFn The original function to run
-   * @param {function} moreFn The extra function to run in the same context after the first
-   * @param {object} scope The context in which to run the fn
-   * @return {function} the new function which will serially call the given functions in the given scope
-   */
-  exports.fnMore = function(originalFn,moreFn,scope){
-      return scope ? function(){
-          originalFn.apply(scope,arguments);
-          moreFn.apply(scope,arguments);
-      } : function(){
-          originalFn();
-          moreFn();
-      };
-  };
+    /**
+     * Get the english ordinal suffix for any number
+     *
+     * @param {number} n number The number to evaluate
+     * @return {string} The ordinal for that number
+     */
+    exports.ord = function(n) {
+        var sfx = ["th", "st", "nd", "rd"],
+            v = n % 100;
+        return sfx[(v - 20) % 10] || sfx[v] || sfx[0];
+    };
+    /**
+     * Generic empty function to speed up supplying anon empty functions.
+     * If you are using jQuery, you could use $.noop if returning undefined is desireable
+     * but this one is useful for anything requiring a boolean true return
+     *
+     * @return {boolean} true
+     */
+    exports.fn = function() {
+        return true;
+    };
+    /**
+     * empty event handler function, which simply prevents default handling
+     */
+    exports.eFn = function(e) {
+        e.preventDefault();
+    };
 
-}(typeof exports === 'object' && exports || (this===window ? window.core={} : this)));
+    /**
+     * get a new function, which runs two functions serially within a given context
+     *
+     * @example
+     *    var fn = core.fnMore(oldFn,newFn,someObj);
+     *    fn(); // runs oldFn, then newFn in the context of someObj
+     * @param {function} originalFn The original function to run
+     * @param {function} moreFn The extra function to run in the same context after the first
+     * @param {object} scope The context in which to run the fn
+     * @return {function} the new function which will serially call the given functions in the given scope
+     */
+    exports.fnMore = function(originalFn, moreFn, scope) {
+        return scope ?
+        function() {
+            originalFn.apply(scope, arguments);
+            moreFn.apply(scope, arguments);
+        } : function() {
+            originalFn();
+            moreFn();
+        };
+    };
+
+    // UMD support
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = exports;
+    } else if (typeof define === "function" && define.amd) {
+        define([], function() {
+            return exports;
+        });
+    }
+
+
+}(typeof exports === 'object' && exports || (this === window ? window.core = {} : this)));
