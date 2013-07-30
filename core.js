b491abfea5ad7338a62d74231ea1be340fbdb48b
Adam Eivy <atomantic@gmail.com>
2013-07-30 21:32:43 +0000

    adding the yeoman build files to gh-pages [ci skip]

--- core.js
+++ core.js
@@ -62,6 +62,11 @@
 }());
 /*jslint browser:true*/
 /**
+ * The ecmascript String prototype
+ * @external String
+ * @see {@link http://www.ecma-international.org/ecma-262/5.1/#sec-15.5.3.1 ECMASCript 5.1 String.prototype}
+ */
+/**
  * Core.js tries to make the web suck less by providing a bunch of
  * tiny methods and library extensions that should be natively available.
  *
@@ -157,11 +162,26 @@
         return (window.console = {});
     }
 }()));
-/**
- * The ecmascript String prototype
- * @external String
- * @see {@link http://www.ecma-international.org/ecma-262/5.1/#sec-15.5.3.1 ECMASCript 5.1 String.prototype}
- */
+if (typeof String.prototype.endsWith !== 'function') {
+    /**
+     * see if a string ends with a given string
+     * Once ecmascript adds this natively, you should build core.js without this method:
+     * @link http://wiki.ecmascript.org/doku.php?id=harmony%3astring_extras
+     * @link http://jsperf.com/string-prototype-endswith/3
+     * @function external:String.prototype.endsWith
+     * @example
+     *  'some string'.endsWith('g') => true;
+     *  'some string'.endsWith('string') => true;
+     *  'some string'.endsWith('!') => false;
+     * @param {string} A substring expected to be in the beginning of this string
+     * @return {boolean}
+     */
+    String.prototype.endsWith = function (suffix){ 
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+}else{
+    console.log('core.js library built with excessive String.prototype.endsWith');
+}
 if (typeof String.prototype.startsWith !== 'function') {
     /**
      * see if a string begins with a given string
@@ -173,8 +193,8 @@ if (typeof String.prototype.startsWith !== 'function') {
      * @param {string} A substring expected to be in the beginning of this string
      * @return {boolean}
      */
-    String.prototype.startsWith = function (str){
-        return this.slice(0, str.length) === str;
+    String.prototype.startsWith = function (prefix){
+        return this.slice(0, prefix.length) === prefix;
     };
 }else{
     console.log('core.js library built with excessive String.prototype.startsWith');
