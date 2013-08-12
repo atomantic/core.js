67683d5ea3ba0ff6b04ca52e243637aca32ac9e4
Adam Eivy <adam.eivy@disney.com>
2013-08-12 16:58:01 -0700

    allowing custom build removal of safe.js and log requirement from string.startsWith and string.endsWith

--- src/string.startsWith.js
+++ src/string.startsWith.js
@@ -1,19 +1,14 @@
-/*global console*/
-if (typeof String.prototype.startsWith !== 'function') {
-    /**
-     * see if a string begins with a given string
-     * 
-     * Once ecmascript adds this natively, you should build core.js without this method:
-     * @link http://wiki.ecmascript.org/doku.php?id=harmony%3astring_extras
-     * @function external:String.prototype.startsWith
-     * @param {string} A substring expected to be in the beginning of this string
-     * @return {boolean}
-      * @example
-      *  'some string'.startsWith('s') === true;
-     */
-    String.prototype.startsWith = function (prefix){
-        return this.slice(0, prefix.length) === prefix;
-    };
-}else{
-    console.log('core.js library built with excessive String.prototype.startsWith');
-}
+/**
+ * see if a string begins with a given string
+ * 
+ * Once ecmascript adds this natively, you should build core.js without this method:
+ * @link http://wiki.ecmascript.org/doku.php?id=harmony%3astring_extras
+ * @function external:String.prototype.startsWith
+ * @param {string} A substring expected to be in the beginning of this string
+ * @return {boolean}
+  * @example
+  *  'some string'.startsWith('s') === true;
+ */
+String.prototype.startsWith = function (prefix){
+    return this.slice(0, prefix.length) === prefix;
+};
